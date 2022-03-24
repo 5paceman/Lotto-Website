1b0ea933e8d04be8f28d1abd96c0b44ba0f4e11b
5paceman <tom.james15@hotmail.com>
2022-03-24 01:19:28 +0000

    init

---
+++ server/index.js
@@ -0,0 +1,12 @@
+const express = require("express");
+
+const PORT = process.env.PORT || 3001;
+
+const app = express();
+
+// Have Node serve the files for our built React app
+app.use(express.static(path.resolve(__dirname, '../client/build')));
+
+app.listen(PORT, () => {
+    console.log(`Server listening on port ${PORT}`);
+});
