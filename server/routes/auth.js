3eaf57fb7185177876b683bcfde7c16f6ff31787
Tom-Surface <tom.james15@hotmail.com>
2022-03-27 19:27:57 +0100

    updated planning to reflect obj changes and wrote auth route

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,5 +1,7 @@
 var express = require("express");
 var router = express.Router();
+var User = require("../model/user");
+var Token = require("../model/token");
 
 
 /* Checks the users credentials and returns a login token
@@ -10,6 +12,26 @@ router.post("/login", function(req, res, next) {
     var username = req.body.username;
     var password = req.body.password;
 
+    var user = User.findOne({username: username});
+    if(user)
+    {
+        user.comparePassword(password, function(err, isMatch) {
+            if(err)
+            {
+                console.log(err);
+            } else {
+                if(isMatch) {
+                    var token = new Token({user: user});
+                    token.save();
+                    res.json({auth_token:token.token});
+                } else {
+                    res.json({error: 'Password incorrect.'});
+                }
+            }
+        });
+    } else {
+        res.json({error: 'Invalid username'});
+    }
 
 });
 
