6f530430de01836b0b56dc98e9ed28968913a163
5paceman <tom.james15@hotmail.com>
2022-04-06 17:50:21 +0100

    fixed errors, finished tests for ticket api

--- server/index.test.js
+++ server/index.test.js
@@ -4,7 +4,7 @@ const app = require('./index');
 const TEST_USERNAME = "test";
 const TEST_PASSWORD = "test10005";
 
-var AUTH_TOKEN = null;
+let AUTH_TOKEN = null;
 
 const genFakeEmail = () => {
     var chars = "abcdefghijklmnopqrstuvwxyz1234567890";
@@ -106,9 +106,8 @@ describe("User API", () => {
     it("should be able to delete user", () => {
         return request(app)
             .post("/api/user/delete")
-            .send({
-                auth_token: TEMP_TOKEN
-            })
+            .set('auth_token', TEMP_TOKEN)
+            .send()
             .expect('Content-Type', /json/)
             .expect(200)
             .then(res => {
@@ -121,8 +120,8 @@ describe("User API", () => {
     it("should be able to update user", () => {
         return request(app)
             .post("/api/user/update")
+            .set('auth_token', AUTH_TOKEN)
             .send({
-                auth_token: AUTH_TOKEN,
                 email: genFakeEmail()
             })
             .expect('Content-Type', /json/)
@@ -136,7 +135,93 @@ describe("User API", () => {
 });
 
 describe("Ticket API", () => {
+    let createdTicketId = null;
+    let winningNumbers = null;
+
+    it("should be able to get latest numbers", () => {
+        return request(app)
+            .get("/api/ticket/latest")
+            .set('auth_token', AUTH_TOKEN)
+            .expect('Content-Type', /json/)
+            .expect(200)
+            .then(res => {
+                expect(res.body).toEqual({
+                    mainNumbers: expect.any(Array),
+                    luckyStars: expect.any(Array)
+                });
+
+                winningNumbers = res.body.mainNumbers.concat(res.body.luckyStars);
+                console.log(winningNumbers);
+            });
+    })
+
     it("should be able to create ticket", () => {
-        
+        return request(app)
+            .post("/api/ticket/create")
+            .set('auth_token', AUTH_TOKEN)
+            .send({
+                users: [],
+                numbers: winningNumbers,
+                expires: Date.now()
+            })
+            .expect('Content-Type', /json/)
+            .expect(200)
+            .then(res => {
+                expect(res.body).toEqual({
+                    ticket_id: expect.any(String)
+                });
+                createdTicketId = res.body.ticket_id;
+            })
     });
+
+    it("should be able to check details on ticket", () => {
+        return request(app)
+            .get(`/api/ticket/get?ticket_id=${createdTicketId}`)
+            .set('auth_token', AUTH_TOKEN)
+            .expect('Content-Type', /json/)
+            .expect(200)
+            .then(res => {
+                expect(res.body).toEqual({
+                    ticket_id: createdTicketId,
+                    numbers: winningNumbers,
+                    expiry: expect.any(String),
+                    users: expect.any(Array),
+                    purchased: expect.any(String)
+                })
+            })
+    });
+
+    it("should be able to check ticket", () => {
+        return request(app)
+            .get(`/api/ticket/check?ticket_id=${createdTicketId}`)
+            .set('auth_token', AUTH_TOKEN)
+            .expect('Content-Type', /json/)
+            .expect(200)
+            .then(res => {
+                expect(res.body).toEqual({
+                    winning: true,
+                    winnings: 'JACKPOT',
+                    winningMainNumbers: 5,
+                    winningLuckyStars: 2
+                });
+            });
+            
+    });
+
+    it("should be able to delete a ticket", () => {
+        return request(app)
+            .post("/api/ticket/delete")
+            .set('auth_token', AUTH_TOKEN)
+            .send({
+                ticket_id: createdTicketId
+            })
+            .expect('Content-Type', /json/)
+            .expect(200)
+            .then(res => {
+                expect(res.body).toEqual({
+                    success: expect.any(String)
+                })
+            })
+    });
+
 });
